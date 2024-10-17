e1df223db72dd8aed4b4b2a6db81266feb836cbd
omar6685 <omr6685@hotmail.com>
2024-10-17 15:22:57 +0300

    gitignore file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
     // res.setHeader('Content-Type', 'text/html');
     // res.statusCode = 404;
 
-    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.writeHead(404, { 'Content-Type': 'application/json' });
 
 
     res.end(JSON.stringify({message: 'server Error'}));
