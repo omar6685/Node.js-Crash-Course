a413a7850f6705d688149bee8d5978bea05c5438
omar6685 <omr6685@hotmail.com>
2024-10-17 14:18:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import getPosts, {getPostsLenth} from "./postController.js";
+console.log(getPosts());
+console.log('Posts length:', getPostsLenth());
+// const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
+// 
+// console.log('Random number:', generateRandomNumber()); 
+// console.log('Celcius to Fahrenheit:', celciusToFahrenheit(0));
