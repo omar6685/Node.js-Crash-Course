4ff3bb574dd83285c8793bb1d0552ee8cc570a7c
omar6685 <omr6685@hotmail.com>
2024-10-19 11:29:13 +0300

    Req Object

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const PORT = process.env.PORT;
 const server = http.createServer((req, res) => {
     // res.setHeader('Content-Type', 'text/html');
     // res.statusCode = 404;
+    console.log(req.url);
+    console.log(req.method);
 
-    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end('<h1>Hello World</h1>');
 
 
-    res.end(JSON.stringify({message: 'server Error'}));
+    // res.end(JSON.stringify({message: 'server Error'}));
 });
 
 
